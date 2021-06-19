43531bbe07d09b8ac657b4b401c4e3b0253451bf
wenzhizhao <wenzhizhao@bytedance.com>
2021-06-19 22:44:07 +0800

    feat: start percolation

---
+++ percolation/PercolationStats.java
@@ -0,0 +1,40 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
+public class PercolationStats {
+
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+
+    }
+
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return 1d;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return 1d;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return 1d;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return 1d;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        return;
+    }
+
+}
